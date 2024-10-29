13f52e8309a5e1b3abb289a878c4123ffb323a2d
HemlockPham7 <london1952hoang@gmail.com>
2024-10-29 08:13:07 +0700

    Chore: Dockerizing Setup

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/HemlockPham7/backend/config"
+	"github.com/HemlockPham7/backend/db"
 	"github.com/HemlockPham7/backend/handlers"
 	"github.com/HemlockPham7/backend/repositories"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	envConfig := config.NewEnvConfig()
+	db := db.Init(envConfig, db.DBMigrator)
+
 	app := fiber.New(fiber.Config{
 		AppName:      "TicketBooking",
 		ServerHeader: "Fiber",
 	})
 
 	// Repositories
-	eventRepository := repositories.NewEventRepository(nil)
+	eventRepository := repositories.NewEventRepository(db)
 
 	// Routing
 	server := app.Group("/api")
 
 	handlers.NewEventHandler(server.Group("/event"), eventRepository)
 
-	app.Listen(":3000")
+	app.Listen(fmt.Sprintf(":" + envConfig.ServerPort))
 }
