37dc1c368784cea71212728da7978b82dfce91b0
HemlockPham7 <london1952hoang@gmail.com>
2024-10-30 16:13:10 +0700

    Feat: identify whose tickets

--- backend/handlers/ticket.go
+++ backend/handlers/ticket.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/HemlockPham7/backend/models"
@@ -20,14 +21,16 @@ func NewTicketHandler(router fiber.Router, repository models.TicketRepository) {
 	router.Get("/", handler.GetMany)
 	router.Post("/", handler.CreateOne)
 	router.Get("/:ticketId", handler.GetOne)
-	router.Put("/validate/:ticketId", handler.ValidateOne)
+	router.Put("/validate", handler.ValidateOne)
 }
 
 func (h *TicketHandler) GetMany(ctx *fiber.Ctx) error {
 	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
 	defer cancel()
 
-	tickets, err := h.repository.GetMany(context)
+	userId := uint(ctx.Locals("userId").(float64))
+
+	tickets, err := h.repository.GetMany(context, userId)
 
 	if err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
@@ -44,12 +47,13 @@ func (h *TicketHandler) GetMany(ctx *fiber.Ctx) error {
 }
 
 func (h *TicketHandler) GetOne(ctx *fiber.Ctx) error {
-	ticketId := ctx.Params("ticketId")
-
 	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
 	defer cancel()
 
-	ticket, err := h.repository.GetOne(context, ticketId)
+	ticketId, _ := strconv.Atoi(ctx.Params("ticketId"))
+	userId := uint(ctx.Locals("userId").(float64))
+
+	ticket, err := h.repository.GetOne(context, userId, uint(ticketId))
 
 	if err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
@@ -68,10 +72,11 @@ func (h *TicketHandler) GetOne(ctx *fiber.Ctx) error {
 }
 
 func (h *TicketHandler) CreateOne(ctx *fiber.Ctx) error {
-	ticket := &models.Ticket{}
-
 	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
 	defer cancel()
+
+	ticket := &models.Ticket{}
+	userId := uint(ctx.Locals("userId").(float64))
 
 	if err := ctx.BodyParser(ticket); err != nil {
 		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(&fiber.Map{
@@ -81,7 +86,7 @@ func (h *TicketHandler) CreateOne(ctx *fiber.Ctx) error {
 		})
 	}
 
-	ticket, err := h.repository.CreateOne(context, ticket)
+	ticket, err := h.repository.CreateOne(context, userId, ticket)
 
 	if err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
@@ -99,14 +104,23 @@ func (h *TicketHandler) CreateOne(ctx *fiber.Ctx) error {
 }
 
 func (h *TicketHandler) ValidateOne(ctx *fiber.Ctx) error {
-	ticketId := ctx.Params("ticketId")
-	validateData := make(map[string]interface{})
-	validateData["entered"] = true
-
 	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
 	defer cancel()
 
-	ticket, err := h.repository.UpdateOne(context, ticketId, validateData)
+	validateBody := &models.ValidateTicket{}
+
+	if err := ctx.BodyParser(validateBody); err != nil {
+		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	validateData := make(map[string]interface{})
+	validateData["entered"] = true
+
+	ticket, err := h.repository.UpdateOne(context, validateBody.OwnerId, validateBody.TicketId, validateData)
 
 	if err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
