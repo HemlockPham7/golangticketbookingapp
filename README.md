f225d913251ea706dd2fb72db60c2ff03ea3c143
HemlockPham7 <157110579+HemlockPham7@users.noreply.github.com>
2024-10-30 12:12:31 +0700

    Chore: Dockerizing Setup (#1)
    * Chore: Dockerizing Setup
    
    * Chore: Dockerizing Setup Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # golangticketbookingapp
+
+### Run app
+docker-compose up -D
+
+### Stop 
+docker-compose down
--- backend/repositories/event.go
+++ backend/repositories/event.go
@@ -2,16 +2,16 @@ package repositories
 
 import (
 	"context"
-	"time"
 
 	"github.com/HemlockPham7/backend/models"
+	"gorm.io/gorm"
 )
 
 type EventRepository struct {
-	db any
+	db *gorm.DB
 }
 
-func NewEventRepository(db any) models.EventRepository {
+func NewEventRepository(db *gorm.DB) models.EventRepository {
 	return &EventRepository{
 		db: db,
 	}
@@ -21,14 +21,21 @@ func (r *EventRepository) GetMany(ctx context.Context) ([]*models.Event, error) 
 	events := []*models.Event{}
 
 	// mock data
-	events = append(events, &models.Event{
-		ID:        "0092345902384",
-		Name:      "Super Event",
-		Location:  "Somewhere",
-		Date:      time.Now(),
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	})
+	// events = append(events, &models.Event{
+	// 	ID:        "0092345902384",
+	// 	Name:      "Super Event",
+	// 	Location:  "Somewhere",
+	// 	Date:      time.Now(),
+	// 	CreatedAt: time.Now(),
+	// 	UpdatedAt: time.Now(),
+	// })
+
+	// return events, nil
+	res := r.db.Model(&models.Event{}).Find(&events)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
 
 	return events, nil
 }
